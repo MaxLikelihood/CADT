6de470a222e38567db60bfcbb76bf6a298c30f44
Fan Jin <fan.jin@mail.mcgill.ca>
2014-02-15 19:24:46 -0500

    Refine function naming & add declaration

--- stack.h
+++ stack.h
@@ -5,7 +5,8 @@ struct Stack;
 typedef struct Stack Stack;
 
 Stack* Stack_Create(size_t n);
-void Push(Stack s, void* e);
-void* Pop(Stack s);
-int Stack_Empty(Stack s);
+void Stack_Destroy(Stack* s);
+void Stack_Push(Stack* s, void* e);
+void* Stack_Pop(Stack* s);
+int Stack_Empty(Stack* s);
 #endif
