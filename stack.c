dd28b80c9894128a07a0ad7f35dc8d6a9075cd28
Fan Jin <fan.jin@mail.mcgill.ca>
2014-02-15 19:36:12 -0500

    Add stack creation implementation

--- stack.c
+++ stack.c
@@ -7,3 +7,11 @@ struct Stack
 	void** e;
 }
 
+Stack* Stack_Create(size_t n)
+{
+	Stack* s = (Stack *)malloc(sizeof(Stack));
+	s->size = n;
+	s->top = 0;
+	s->e = (void**)malloc(sizeof(void*) * (n + 1));
+	return s;
+}
