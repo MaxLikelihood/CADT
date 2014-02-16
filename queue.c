95472f5293f62041cae37f524670e70439e27bce
Fan Jin <fan.jin@mail.mcgill.ca>
2014-02-15 23:25:43 -0500

    Add missing size indicator member in struct

--- queue.c
+++ queue.c
@@ -4,12 +4,14 @@ struct Queue
 {
 	size_t head;
 	size_t tail;
+	size_t size;
 	void** e;
 };
 
 Queue* Queue_Create(size_t n)
 {
 	Queue* q = (Queue *)malloc(sizeof(Queue));
+	q->size = n;
 	q->head = q->tail = 1;
 	q->e = (void **)malloc(sizeof(void*) * (n + 1));
 }
