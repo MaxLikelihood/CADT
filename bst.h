8c7971542f8cdd52b4c570978a036f288bfb3dee
Fan Jin <fan.jin@mail.mcgill.ca>
2014-02-20 20:31:26 -0500

    Add missing key comparison function parameter
    Separate key comparison functions required
        Equality
        Order

--- bst.h
+++ bst.h
@@ -11,7 +11,7 @@ BSTNode* BSTNode_Create(void* k);
 void BST_Inorder_Tree_Walk(BST* T, void (f)(void*));
 void BST_Preorder_Tree_Walk(BST* T, void (f)(void*));
 void BST_Postorder_Tree_Walk(BST* T, void (f)(void*));
-BSTNode* BST_Tree_Search(BST* T, void* k, int (f)(void*, void*));
-BSTNode* BST_Iterative_Tree_Search(BST* T, void* k, int (f)(void*, void*));
+BSTNode* BST_Tree_Search(BST* T, void* k, int (f)(void*, void*), int (g)(void*, void*));
+BSTNode* BST_Iterative_Tree_Search(BST* T, void* k, int (f)(void*, void*), int (g)(void*, void*));
 
 #endif
