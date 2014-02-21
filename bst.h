2aaf58fe9d661c11e477adbae1ae322bc0b1af1d
Fan Jin <fan.jin@mail.mcgill.ca>
2014-02-20 21:52:36 -0500

    Fix BST Tree Order Traversal parameters
    For arbitrary node instead of only root

--- bst.h
+++ bst.h
@@ -8,9 +8,9 @@ typedef struct BST BST;
 
 BST* BST_Create(void);
 BSTNode* BSTNode_Create(void* k);
-void BST_Inorder_Tree_Walk(BST* T, void (f)(void*));
-void BST_Preorder_Tree_Walk(BST* T, void (f)(void*));
-void BST_Postorder_Tree_Walk(BST* T, void (f)(void*));
+void BST_Inorder_Tree_Walk(BSTNode* n, void (f)(void*));
+void BST_Preorder_Tree_Walk(BSTNode* n, void (f)(void*));
+void BST_Postorder_Tree_Walk(BSTNode* n, void (f)(void*));
 BSTNode* BST_Tree_Search(BST* T, void* k, int (f)(void*, void*), int (g)(void*, void*));
 BSTNode* BST_Iterative_Tree_Search(BST* T, void* k, int (f)(void*, void*), int (g)(void*, void*));
 BSTNode* BST_Tree_Minimum(BSTNode* n);
